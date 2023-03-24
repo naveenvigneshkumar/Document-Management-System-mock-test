44e97037d5f459b66e5f8e11d2544f9fc3391ba7
Naveen vignesh kumar <naveenvignesh.kumar@gmail.com>
2023-03-24 17:55:05 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    role: {
+        admin: "admin",
+        guest: "guest",
+        reviewer: "reviewer"
+    },
+    status: ['approved', 'unapproved', 'reject'],
+    baseUrl: 'http://localhost:3000/',
+    emailSubject: {
+        notification:"New File Assigned"
+    },
+    adminEmail: 'naveen.sankara@aspiresys.com',
+    reviewerEmail: 'naveenaspiresys123@gmail.com'
+}
